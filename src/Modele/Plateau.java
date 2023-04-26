3e7dd8e5ece8162b26795ab0d5910a8d68a4b0ed
Juloos <juloos.sfr@gmail.com>
2023-04-26 11:01:56 +0200

    Random nextInt bound correction

--- src/Modele/Plateau.java
+++ src/Modele/Plateau.java
@@ -18,7 +18,7 @@ public class Plateau {
         Coord c = new Coord();
         for (c.q = 0; c.q < qMax; c.q++)
             for (c.r = 0; c.r < rMax; c.r++)
-                set(c, weights[rand.nextInt(0, 6)]);
+                set(c, weights[rand.nextInt(7) - 1]);
     }
 
     public void set(Coord c, int val) {
