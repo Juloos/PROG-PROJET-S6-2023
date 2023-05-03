c506bde1e462df7c2aa6d32786e46fa0183b4fcf
BossyRomain <romain.bossy.01@gmail.com>
2023-05-03 13:19:28 +0200

    Ajout de l'affichage du plateau de jeu.

--- src/IHM/Graphique/PlateauGraphique.java
+++ src/IHM/Graphique/PlateauGraphique.java
@@ -9,9 +9,9 @@ import java.util.Random;
 
 public class PlateauGraphique extends JComponent {
 
+    final int BORDURES_X = 2, BORDURES_Y = 1;
     Image[] sprites;
-
-    double TAILLE_CASES_X, TAILLE_CASES_Y, Y_OFFSET;
+    double TAILLE_CASES, Y_OFFSET;
 
     public PlateauGraphique() {
         super();
@@ -37,31 +37,33 @@ public class PlateauGraphique extends JComponent {
         Graphics2D drawable = (Graphics2D) g;
         Random random = new Random();
 
-        TAILLE_CASES_X = Math.sqrt(3.0) * 1000.0 / 8.0 * 2.0;
-        TAILLE_CASES_Y = 2 * 1000.0 / 8.0 * 2.0;
+        final int NB_TUILES_VERT = 8;
 
-        System.out.println("Y offset : " + Y_OFFSET);
+        System.out.println(getHeight());
+        TAILLE_CASES = (getHeight() * 4.0 / (3.0 * NB_TUILES_VERT)) - 1.0;
+
+        Y_OFFSET = TAILLE_CASES / NB_TUILES_VERT;
 
         drawable.setColor(Color.red);
-        for (int i = -5; i < 13; i++) {
-            for (int j = -8; j < (i % 2 == 0 ? 19 : 20); j++) {
+        for (int i = -BORDURES_Y; i < NB_TUILES_VERT + BORDURES_Y; i++) {
+            for (int j = -BORDURES_X; j < (i % 2 == 0 ? NB_TUILES_VERT + BORDURES_X - 1 : NB_TUILES_VERT + BORDURES_X); j++) {
                 int x = XHexToPixel(i, j);
                 int y = YHexToPixel(i);
 
-//                drawable.setColor(random.nextBoolean() ? Color.red : Color.blue);
-//                drawable.fillRect(x, y, (int) TAILLE_CASES, (int) TAILLE_CASES);
+                Image img = (i >= 0 && i < NB_TUILES_VERT && j >= 0 && j < (i % 2 == 0 ? NB_TUILES_VERT - 1 : NB_TUILES_VERT)) ? sprites[random.nextInt(sprites.length - 1) + 1] : sprites[0];
 
-                Image img = (i >= 0 && i < 8 && j >= 0 && j < (i % 2 == 0 ? 7 : 8)) ? sprites[random.nextInt(sprites.length - 1) + 1] : sprites[0];
-                drawable.drawImage(img, x, y, (int) TAILLE_CASES_X, (int) TAILLE_CASES_Y, null);
+                if (img != null) {
+                    drawable.drawImage(img, x, y, (int) TAILLE_CASES, (int) TAILLE_CASES, null);
+                }
             }
         }
     }
 
     private int XHexToPixel(int q, int r) {
-        return (int) ((TAILLE_CASES_X / 2.0) + (TAILLE_CASES_X / 2.2) * Math.sqrt(3.0) * (r - 0.5 * (q & 1)));
+        return (int) ((TAILLE_CASES / 2.0) + (TAILLE_CASES / 2.2) * Math.sqrt(3.0) * (r - 0.5 * (q & 1)));
     }
 
     private int YHexToPixel(int q) {
-        return (int) (Y_OFFSET + (TAILLE_CASES_Y / 2.2) * (3.0 / 2.0) * q);
+        return (int) (Y_OFFSET + (TAILLE_CASES / 2.2) * (3.0 / 2.0) * q);
     }
 }
