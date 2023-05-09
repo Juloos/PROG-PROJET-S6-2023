29aacac328c427c25ffccbed2511e636f955ce01
BossyRomain <romain.bossy.01@gmail.com>
2023-05-09 13:02:44 +0200

    Modification IHM.

--- src/IHM/IHM.java
+++ src/IHM/IHM.java
@@ -4,11 +4,12 @@ import Controleur.MoteurJeu;
 import Modele.Actions.Action;
 import Modele.Jeu.Jeu;
 
-public abstract class IHM implements Runnable {
+public abstract class IHM extends Thread {
 
     private MoteurJeu moteurJeu;
 
     public IHM(MoteurJeu moteurJeu) {
+        super();
         this.moteurJeu = moteurJeu;
     }
 
