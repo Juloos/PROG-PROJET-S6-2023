00820fe2120abb377c2e2ea34aeb0afb5065ecc7
theo koehler <theokoehler.38160@gmail.com>
2023-05-16 16:48:09 +0200

    H10 et H9 en plus

---
+++ src/Modele/IA/H9.java
@@ -0,0 +1,36 @@
+package Modele.IA;
+
+import Modele.Coord;
+import Modele.Jeux.Jeu;
+
+import java.util.ArrayList;
+
+public class H9 implements Heuristique {
+    @Override
+    public double evaluer(Jeu j, int pdvJoueur) {
+        int nbpoissonpdv = 0;
+        int nbpoissonadversairemax = 0;
+        for (int joueur = 0; joueur<j.getNbJoueurs(); joueur++) {
+            ArrayList<Coord> dejavisiter = new ArrayList<>();
+            for (Coord pion : j.getJoueur(joueur).getPions()) {
+                for (Coord c : j.deplacementsPion(pion)) {
+                    if (!dejavisiter.contains(c)) {
+                        dejavisiter.add(c);
+                    }
+                }
+            }
+            int nbpoissonjoueur=0;
+            for (Coord n : dejavisiter){
+                nbpoissonjoueur += j.getPlateau().get(n);
+            }
+            if (joueur==pdvJoueur) {
+                nbpoissonpdv =nbpoissonjoueur;}
+            else {
+                if (nbpoissonjoueur > nbpoissonadversairemax) {
+                    nbpoissonadversairemax = nbpoissonjoueur;
+                }
+            }
+        }
+        return nbpoissonpdv-nbpoissonadversairemax;
+    }
+}
